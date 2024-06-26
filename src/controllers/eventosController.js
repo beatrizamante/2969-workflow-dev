23866d1d6391c732df0a47b4579dc72a3ebf8158
beatrizamante <beatrizamante@hotmail.com>
2024-06-26 14:05:25 -0300

    Trust

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,7 +1,7 @@
 import Evento from '../models/evento.js';
 
 class EventosController {
-  static liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
+  static liberaAcessoEventos = () => unleash.isEnabled('eventos');
 
   static listarEventos = async (req, res) => {
     if (this.liberaAcessoEventos()) {
